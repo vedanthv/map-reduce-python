0c7ff1ec4d9a088d9de7164fa9c86a5ba139a910
vedanthv <vedanthvbaliga@gmail.com>
2023-04-25 22:41:34 +0530

    feature:data cleaning

--- implementation.py
+++ implementation.py
@@ -1,5 +1,14 @@
-inputfile = open(r'D:\University\Semester 6\map-reduce-python\input.txt',"r")
+import re
 
 # Reading the input
+inputfile = open(r'D:\University\Semester 6\map-reduce-python\input.txt',"r")
 text = inputfile.read()
+
+# Data Cleaning
+def data_clean(text):
+  NoNumbers = ''.join([i for i in text if not i.isdigit()]) # Removing numbers
+  NoNumbers = text.lower() # Making the text to lower case
+  onlyText = re.sub(r"[^a-z\s]+",' ',NoNumbers) # Removing punctuation
+  finaltext = "".join([s for s in onlyText.strip().splitlines(True) if s.strip()]) # Removing the null lines
+  return finaltext
 
